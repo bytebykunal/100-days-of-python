5f067cfb6560d2efbdf81057fcffdfa6c8ce0150
bytebykunal <kunalthakur8902@gmail.com>
2026-07-09 18:02:27 +0530

    Add Day 35 Rain Alert

---
+++ Day-35-Rain-Alert/main.py
@@ -0,0 +1,43 @@
+import requests
+from twilio.rest import Client
+import os
+
+MY_LAT = 28.251354
+MY_LONG = 77.853882
+
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+api_key = os.environ.get("OWM_API_KEY")
+account_sid = os.environ.get("ACCOUNT_SID")
+auth_token = os.environ.get("AUTH_TOKEN")
+
+
+
+parameters = {
+    "lat": MY_LAT,
+    "lon": MY_LONG,
+    "appid": api_key,
+    "cnt": 4
+}
+
+response = requests.get(url=OWM_Endpoint, params=parameters)
+response.raise_for_status()
+
+weather_data = response.json()
+
+
+will_rain = False
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    if condition_code < 700:
+        will_rain = True
+        
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body="It's going to rain today. Remember to bring an ☔",
+        from_="+16812756849",
+        to="+919259512414",
+    )
+    print(message.status)
+else:
+    print("No rain expected. No SMS sent.")
